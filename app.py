91f5c470d1bed1efc10dff73f9e11484b0fddb53
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-09-13 04:42:03 +0900

    #1 get 수정 & 메모 틀 잡기

--- app.py
+++ app.py
@@ -34,6 +34,5 @@ def view_memos():
     todo_list = list(db.memoList.find({}, {'_id': False}))
     return jsonify({'todo_list': todo_list})
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
